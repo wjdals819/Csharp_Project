a2778b204fb7bd4a2ae1f0027f478a0ea194ed92
wjdals819 <wjdals819@github.com>
2023-04-15 19:33:47 +0900

    증가, 감소 연산자 실습

---
+++ Inc_Dec_Operator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project
+{
+    class Inc_Dec_Operator
+    {
+        static void Main(string[] args) 
+        {
+            int a = 100;
+            Console.WriteLine(a++);
+            Console.WriteLine(a--);
+
+            int b = 100;
+            Console.WriteLine(++b);
+            Console.WriteLine(--b);
+        }
+    }
+}
