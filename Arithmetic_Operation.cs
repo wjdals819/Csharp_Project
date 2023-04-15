50e514e6c20192a271697d575524c8e2b29a27db
wjdals819 <wjdals819@github.com>
2023-04-15 18:00:05 +0900

    산술 연산자 예제 코드

---
+++ Arithmetic_Operation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project
+{
+    class Arithmetic_Operation
+    {
+        static void Main(string[] args)
+        {
+            int a = 100 + 200;
+            Console.WriteLine($"a: {a}");
+
+            int b = a - 50;
+            Console.WriteLine($"b: {b}");
+
+            int c = b * 10;
+            Console.WriteLine($"c: {c}");
+
+            int d = 10 / 5;
+            Console.WriteLine($"d: {d}");
+
+            int e = 10 % 3;
+            Console.WriteLine($"e: {e}");
+        }
+    }
+}
