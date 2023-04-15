3f240b6d17e1a632cdfd60d9ace9dfd2af9de914
wjdals819 <wjdals819@github.com>
2023-04-15 19:58:47 +0900

    관계 연산자 예제 코드

---
+++ Relational_Operator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project
+{
+    class Relational_Operator
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine($"1>2: {a > b}");
+            Console.WriteLine($"1>=2: {a >= b}");
+            Console.WriteLine($"1<2: {a<b}");
+            Console.WriteLine($"1<=2: {a <= b}");
+            Console.WriteLine($"1==2: {a == b}");
+            Console.WriteLine($"1!=2: {a != b}");
+
+        }
+    }
+}
