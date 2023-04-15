aff6abae72ef54c7336d935d166b8e31993f0213
wjdals819 <wjdals819@github.com>
2023-04-15 20:10:54 +0900

    조건 연산자 예제 코드

---
+++ Conditional_Operator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project
+{
+    class Conditional_Operator
+    {
+        static void Main(string[] args)
+        {
+            string result = 10 - 5 > 3 ? "맞습니다" : "틀립니다";
+
+            Console.WriteLine($"10-5는 3보다 크다:{result}");
+
+        }
+    }
+}
