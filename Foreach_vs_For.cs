a78f401ceab5ce85d9a12ee0347c49e0865d78c8
wjdals819 <wjdals819@github.com>
2023-08-07 21:28:33 +0900

    For문vsForeach문

---
+++ Foreach_vs_For.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project
+{
+    class Foreach_vs_For
+    {
+        static void Main(string[] args)
+        {
+            string[,] menu = new string[,] { { "떡볶이", "순대","튀김","치즈라면"},{ "참치김밥", "냉모밀", "쫄면", "돈까스" }};
+
+            Console.WriteLine("<for문 사용>");
+
+            for(int i = 0; i < menu.GetLength(0); i++)
+            {            
+                for (int j = 0; j < menu.GetLength(1); j++)
+                {
+                    Console.Write(menu[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("<foreach문 사용>");
+
+            foreach(string a in menu)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
