2ed652b89c2abc9bbd912c44b196649078d10d75
wjdals819 <wjdals819@github.com>
2023-08-19 16:50:41 +0900

    Static 필드 예제

---
+++ Static.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Project
+{
+    class CountA
+    {
+        public static int Count = 0;
+    }
+
+    class CountB
+    {
+        public CountB()
+        {
+            CountA.Count++;
+            //생성자
+        }
+    }
+
+    class CountC
+    {
+        public CountC()
+        {
+            CountA.Count++;
+            //생성자
+        }
+    }
+
+    class MainApp
+    {
+        static void Main()
+        {
+            Console.WriteLine($"CountA의 Count: {CountA.Count}");
+
+            new CountB(); //생성자
+            new CountC(); //생성자
+            new CountB(); //생성자
+            new CountC(); //생성자
+
+            Console.WriteLine($"CountA의 Count: {CountA.Count}");
+        }
+    }
+}
